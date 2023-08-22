db1d66e2212d6b00a1fd989004de5abdad835ec7
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-08-22 09:30:22 +0900

    Initial Commit

---
+++ Week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week6
+//
+//  Created by 서동운 on 8/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
